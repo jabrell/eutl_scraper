d8d3252ead764aadb86e87b9dddbbb08ffd0e9c9
Jan <abrell@posteo.de>
2022-05-25 22:23:25 +0200

    Added spider for entitlement data

--- eutl_scraper/items/__init__.py
+++ eutl_scraper/items/__init__.py
@@ -1,3 +1,4 @@
 from .accountItems import AccountItem, ContactItem, AccountIDMapItem
-from .installationItems import InstallationItem, ComplianceItem, SurrenderingDetailsItem
+from .installationItems import (InstallationItem, ComplianceItem, 
+    SurrenderingDetailsItem, EntitlementItem)
 from .transactionItems import TransactionItem, TransactionBlockItem
--- eutl_scraper/pipelines.py
+++ eutl_scraper/pipelines.py
@@ -3,22 +3,26 @@ from itemadapter import ItemAdapter
 from eutl_scraper.items import (AccountItem, ContactItem, 
                                 InstallationItem, ComplianceItem, 
                                 SurrenderingDetailsItem, TransactionItem,
-                                TransactionBlockItem, AccountIDMapItem)
+                                TransactionBlockItem, AccountIDMapItem,
+                                EntitlementItem)
 from eutl_scraper.own_settings import DIR_PARSED
 import os.path
 
 
 
 class EutlScraperPipeline:
+    # all items to process
+    # if new items, add them to list for post-processing
     itemsToProcess = [
         {"item": AccountItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'accounts.csv', "appendExisting": False},
         {"item": ContactItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'contacts.csv', "appendExisting": False},
         {"item": InstallationItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'installations.csv', "appendExisting": False},
         {"item": ComplianceItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'compliance.csv', "appendExisting": False},
         {"item": SurrenderingDetailsItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'surrendering.csv', "appendExisting": False},
-        {"item": TransactionItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'transactions.csv', "appendExisting": True},
-        {"item": TransactionBlockItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'transactionBlocks.csv', "appendExisting": True},
-        {"item": AccountIDMapItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'accountIdMap.csv', "appendExisting": True},
+        {"item": TransactionItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'transactions.csv', "appendExisting": False},
+        {"item": TransactionBlockItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'transactionBlocks.csv', "appendExisting": False},
+        {"item": AccountIDMapItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'accountIdMap.csv', "appendExisting": False},
+        {"item": EntitlementItem, "rows": [], "header": [], "output_file_name": DIR_PARSED + 'entitlements.csv', "appendExisting": False},
     ]
 
     def process_item(self, item, spider):
@@ -27,7 +31,7 @@ class EutlScraperPipeline:
                 adapter = dict(ItemAdapter(item))
                 it["rows"].append(adapter)
                 it["header"] = [k for k in item.fields.keys()]
-
+                
     def close_spider(self, spider):
         for it in self.itemsToProcess:
             if len(it["rows"]) == 0:
